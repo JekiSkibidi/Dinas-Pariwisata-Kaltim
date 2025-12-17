8fa25babd783350e3b7576a463af4991e46f553f
JekiSkibidi <dzaky020705@gmail.com>
2025-12-17 16:32:59 +0800

    Fix media upload: use STORAGES setting for Django 6.0, remove MEDIA_ROOT fallback

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,12 +164,18 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL.replace("https://", "")}'
 
-# Use S3 for media files
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# Django 6.0+ uses STORAGES setting
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media URL pointing to Supabase Storage
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')  # Fallback for local
 
 LOGIN_REDIRECT_URL = '/authentikasi/login'
 LOGOUT_REDIRECT_URL = '/'
