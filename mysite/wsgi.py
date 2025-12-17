b18913d289be6b9f3e0113b37f42c0d8e539f86e
JekiSkibidi <dzaky020705@gmail.com>
2025-12-17 14:53:19 +0800

    Fix vercel build - remove static build step

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,10 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# Collect static files on first request (Vercel serverless)
+try:
+    from django.core.management import call_command
+    if os.environ.get('VERCEL'):
+        call_command('collectstatic', '--noinput', '--clear')
+except Exception as e:
+    print(f"Static files collection skipped: {e}")
 
 application = get_wsgi_application()
 app = application
