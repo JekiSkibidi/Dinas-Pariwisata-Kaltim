358e6344a2a68d93ea8c36658802ecedc51107a0
JekiSkibidi <dzaky020705@gmail.com>
2025-12-17 15:58:04 +0800

    Fix static files: use build_files.sh with static-build

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Collect static files
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build completed successfully!"
